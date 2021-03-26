b758a285aded58f916bb4088f74962b73f027667
sergio <sergio@teste.com>
2021-03-26 20:02:16 -0300

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    TesteComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
